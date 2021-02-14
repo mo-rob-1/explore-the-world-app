dd41c733d6e48f272e8700fb62d29fb77a8e2289
Mo Rob <31892724+morob1@users.noreply.github.com>
2021-02-14 12:22:33 +0000

    update the class name in header.js

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
     return (
         <div>
-            <h1 className="header__title">Explore the World</h1>
+            <h1 className="title">Explore the World</h1>
         </div>
     )
 }
