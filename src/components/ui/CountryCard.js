2434f458ad43e4b3e5634e8249f20cc08d621a40
Mo Rob <31892724+morob1@users.noreply.github.com>
2021-02-15 17:40:06 +0000

    add country card component

---
+++ src/components/ui/CountryCard.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const CountryCard = (props) => {
+    const { name, flag, population, capital, region, demonym } = props;
+    return (
+        <div className="card">
+            <div className="card-img-container">
+                <img className='card-img' src={flag} alt={name} />
+            </div>
+            <div className="card-info-container">
+                <ul>
+                    <li>
+                        <strong>Country:</strong> {name} 
+                    </li>
+                    <li>
+                        <strong>Capital:</strong> {capital}
+                    </li>
+                    <li>
+                        <strong>Region:</strong> {region}
+                    </li>
+                    <li>
+                        <strong>Population:</strong> {population}
+                    </li>
+                    <li>
+                        <strong>Demonym:</strong> {demonym}
+                    </li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default CountryCard;
