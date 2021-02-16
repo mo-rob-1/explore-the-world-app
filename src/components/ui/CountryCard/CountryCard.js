6193246410c971e98a0698128e44c1e8fe485e59
Mo Rob <31892724+morob1@users.noreply.github.com>
2021-02-16 16:49:29 +0000

    update country card

--- src/components/ui/CountryCard/CountryCard.js
+++ src/components/ui/CountryCard/CountryCard.js
@@ -1,33 +1,37 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 import './CountryCard.css';
 
 const CountryCard = (props) => {
     const { name, flag, population, capital, region, demonym } = props;
+
     return (
-        <div className="card">
-            <div className="card-img-container">
-                <img className='card-img' src={flag} alt={name} />
+        <Link to={`/${name}`}>
+            <div className="card">
+                <div className="card-img-container">
+                    <img className='card-img' src={flag} alt={name} />
+                </div>
+                <div className="card-info-container">
+                    <ul>
+                        <li>
+                            <strong>Country:</strong> {name} 
+                        </li>
+                        <li>
+                            <strong>Capital:</strong> {capital}
+                        </li>
+                        <li>
+                            <strong>Region:</strong> {region}
+                        </li>
+                        <li>
+                            <strong>Population:</strong> {population}
+                        </li>
+                        <li>
+                            <strong>Demonym:</strong> {demonym}
+                        </li>
+                    </ul>
+                </div>
             </div>
-            <div className="card-info-container">
-                <ul>
-                    <li>
-                        <strong>Country:</strong> {name} 
-                    </li>
-                    <li>
-                        <strong>Capital:</strong> {capital}
-                    </li>
-                    <li>
-                        <strong>Region:</strong> {region}
-                    </li>
-                    <li>
-                        <strong>Population:</strong> {population}
-                    </li>
-                    <li>
-                        <strong>Demonym:</strong> {demonym}
-                    </li>
-                </ul>
-            </div>
-        </div>
+        </Link>
     )
 }
 
