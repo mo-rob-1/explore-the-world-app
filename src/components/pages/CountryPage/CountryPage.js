15059a19a06378db3ae7f154bc33c366487e9751
Mo Rob <31892724+morob1@users.noreply.github.com>
2021-02-17 11:12:35 +0000

    update country page to include css classes

--- src/components/pages/CountryPage/CountryPage.js
+++ src/components/pages/CountryPage/CountryPage.js
@@ -19,33 +19,33 @@ const CountryPage = () => {
             {country.map((c) => {
             const {numericCode, flag, name, population, region, capital, nativeName, subregion, borders, languages, currencies, demonym, topLevelDomain} = c
             return (
-                <article key={numericCode}>
-                    <div>
+                <div key={numericCode}>
                     <Link to={`/`}>Back</Link>
-                        <h1>{name}</h1>
+                    <div className="country-pg__container">
                         <img src={flag} alt={name} />
-                        <h2>{population}</h2>
-                        <h2>{region}</h2>
-                        <h2>{capital}</h2>
-                        <h2>{nativeName}</h2>
-                        <h2>{subregion}</h2>
-                        <h2>{topLevelDomain}</h2>
-                        <h2>{currencies[0].name}</h2>
-                        <h2>{demonym}</h2>
-                        <h2>{languages[0].name}</h2>
+                        <h1 className="country-pg__name">Name: {name}</h1>
+                        <h4 className="country-pg__native-name">Native Name: {nativeName}</h4>
+                        <h4 className="country-pg__population">Population: {population}</h4>
+                        <h4 className="country-pg__region">Region: {region}</h4>
+                        <h4 className="country-pg__sub-region">Sub-Region: {subregion}</h4>
+                        <h4 className="country-pg__capital">Capital: {capital}</h4>
+                        <h4 className="country-pg__domain">Top-Level Domain: {topLevelDomain}</h4>
+                        <h4 className="country-pg__currency">Currency: {currencies[0].name}</h4>
+                        <h4 className="country-pg__demonym">Demonym: {demonym}</h4>
+                        <h4 className="country-pg__language">Language: {languages[0].name}</h4>
                         
-                        <div className="borders">
+                        <div className="country-pg__border-countries-container">
+                        <h3 className="country-pg__border-countries">Border Countries:</h3>
                         {borders.map((border) => {
                             return (
-                                <ul key={border}>
-                                    <li>{border}</li>
+                                <ul className="country-pg__border-list-container" key={border}>
+                                    <li className="country-pg__border-list">{border}</li>
                                 </ul>
                             )
                         })}
                         </div>
-                      
                     </div>
-                </article>
+                </div>
             )
             })}
         </div>
