e109f3b689aa436458db9632185e28c846b839eb
Mo Rob <31892724+morob1@users.noreply.github.com>
2021-02-18 13:57:35 +0000

    debug header component

--- src/components/ui/Header/Header.js
+++ src/components/ui/Header/Header.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Header.css';
 
 const Header = () => {
     return (
         <header className="header">
-            <h1 className="header__title">Explore the World</h1>
+            <Link className="header__link" to="/"><h1 className="header__title">Explore the World</h1></Link>
         </header>
     )
 }
